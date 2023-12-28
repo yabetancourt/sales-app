3ed0ba25f24d330cd79339d635d54484c6623b83
yabetancourt <yabetancourt@uclv.cu>
2023-12-28 14:26:40 -0500

    Added swagger and sprind data rest

--- src/main/java/com/yabetancourt/sales/repositories/ProductRepository.java
+++ src/main/java/com/yabetancourt/sales/repositories/ProductRepository.java
@@ -2,11 +2,11 @@ package com.yabetancourt.sales.repositories;
 
 import com.yabetancourt.sales.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@Repository
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameContainsIgnoreCase(String name);
 }
